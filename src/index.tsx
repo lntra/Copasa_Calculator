abc4e9fe3af16c549fe00eeb79fb7248c3902df9
https://github.com/lntra <fernandoaraujo1202@gmail.com>
2022-11-30 21:03:27 -0300

    f

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App/>
     </BrowserRouter>
   </React.StrictMode>
